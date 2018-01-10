128ea5ce3220d52ebc0017c03d4c317fb77e6f2a
Rashan Hodge <rashan.hodge@gmail.com>
2018-01-10 01:24:09 -0800

    Beginning pseudocode

--- mqttApp/publisher.js
+++ mqttApp/publisher.js
@@ -1,1 +1,12 @@
 //The device which publishes messages on a particular topic.
+
+//NOTE: The Pi AND App deployed to Heroku will act as subscribers. 
+//The pi will notify App that message has been acknowledged. The app will subscribe to message acks.
+
+
+//BEGINNING STEPS:
+//Send a Connect packet to the broker (With Authentication if needed)
+
+//Wait for Connect acknowledgement packet from the broker
+
+//and when Connect acknowledgement is received , send the publish message packet with appropriate topic name
--- mqttApp/subscriber.js
+++ mqttApp/subscriber.js
@@ -1,1 +1,14 @@
 //The device which receives messages by subscribing to a particular topic.
+
+//NOTE: The Pi AND App deployed to Heroku will act as subscribers. 
+//The pi will listen for new messages. The app will listen for message acknowledgements from the pi.
+
+
+//BEGINNING STEPS:
+//Send a Connect packet to the broker with auth
+
+// Wait for Connect acknowledgement packet from the broker
+
+// and when Connect acknowledgement is received , send the subscribe packet with appropriate topic name
+
+// Wait for the subscribe acknowledgement packet
